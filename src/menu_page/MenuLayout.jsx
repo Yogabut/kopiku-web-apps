dc96dc2897e2e18b43c286d87f05c4262679ced2
I Gede Yoga Asta Pratama <yoga.asta.pra@gmail.com>
2025-06-28 20:20:17 +0800

    frontend versi 1

---
+++ src/menu_page/MenuLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MenuLayout = () => {
+    return (
+        <div>
+            {/* Menu Layout Content */}
+        </div>
+    );
+}
+export default MenuLayout;
