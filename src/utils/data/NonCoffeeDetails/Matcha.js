9f74579e830dff316aa4dbf1952e6e731f480346
I Gede Yoga Asta Pratama <yoga.asta.pra@gmail.com>
2025-07-01 21:44:43 +0800

    adding details data for food & non coffee menu

---
+++ src/utils/data/NonCoffeeDetails/Matcha.js
@@ -0,0 +1,30 @@
+const MatchaDetails = [
+  {
+    id: 1,
+    name: "Matcha Latte",
+    category: "Matcha",
+    type: ["hot", "iced"],
+    description: "Creamy and earthy Japanese matcha blended with milk, served hot or cold.",
+    price: 28000,
+    image: ""
+  },
+  {
+    id: 2,
+    name: "Iced Matcha",
+    category: "Matcha",
+    type: ["iced"],
+    description: "Chilled matcha drink with a refreshing green tea kick, served over ice.",
+    price: 28000,
+    image: ""
+  },
+  {
+    id: 3,
+    name: "Matcha Lemonade",
+    category: "Matcha",
+    type: ["iced"],
+    description: "A zesty fusion of matcha and lemonade — energizing and refreshing.",
+    price: 30000,
+    image: ""
+  }
+]
+export default MatchaDetails;
